5cb1240975e147bf14e4c22b9e5674a459fab89e
Josue Cruz <cruzlopez.josuedavid96@gmail.com>
2023-06-03 23:24:42 -0400

    Added models and admin site for car and model make

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,14 +4,46 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# <HINT> Create a Car Make model `
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default="Toyota")
+    description = models.CharField(max_length=1000)
+
+
+    def __str__(self):
+        return "Name: " + self.name + "\n" + "Description: " + self.description
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+class CarModel(models.Model):
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30, default="2000GT")
+    dealerID = models.IntegerField()
+    SEDAN = 'Sedan'
+    SUV ='SUV'
+    WAGON = 'WAGON'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    ]
+    carType = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=SEDAN
+    )
+
+    date = models.DateField(null=True)
+
+    def __str__(self):
+        return  "Car Maker: " + self.carmake.name + "," + \
+                "Name: " + self.name + "," + \
+                "Type: " + self.carType + "," 
+
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
